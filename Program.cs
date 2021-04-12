a904e26db64ec20db3315d631da47a4318e7eecc
Barre <fred03.blomberg@gmail.com>
2021-04-12 10:58:07 +0200

    Added card deck and symbols

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
 
 namespace HighOrLow
 {
@@ -6,7 +9,68 @@ namespace HighOrLow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+	    Console.OutputEncoding = System.Text.Encoding.Default;
+
+
+	    
+
+	    Console.WriteLine("Hello World!");
+
+            // Value test = Value.king;
+            // Console.WriteLine(test);
+
+            var suit = RandomEnumValue<Suit>();
+            var value = RandomEnumValue<Value> ();
+	    
+	    Console.WriteLine (value.ToString () +" of " + suit.ToString());
+
+
+	    string a = "\u2660";  
+	    Console.WriteLine(a);  
+
+
         }
+
+	static Random _R = new Random ();
+	static T RandomEnumValue<T> ()
+	{
+	    var v = Enum.GetValues (typeof (T));
+	    return (T) v.GetValue (_R.Next(v.Length));
+	}
+
+        enum Value
+        {
+	    Ace,
+	    Two,
+	    Three,
+	    Four,
+	    Five,
+	    Six,
+	    Seven,
+	    Eight,
+	    Nine,
+	    Ten,
+            Jack,
+            Queen,
+            King
+        }
+
+
+        enum Suit {
+	    Hearts,
+	    Clubs,
+	    Diamonds,
+	    Spades
+        }
+
+        static void writeScore() {
+
+	    string jsonString = "Hej";
+
+	    File.WriteAllText("./test.txt", jsonString);
+	    
+	}
+
     }
 }
