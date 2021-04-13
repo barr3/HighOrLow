43a244c4f99d0aa7b8e14d7800999eda7a06bc60
Barre <fred03.blomberg@gmail.com>
2021-04-13 13:19:59 +0200

    Remove cards when they are drawn.

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ namespace HighOrLow
     class Program
     {
 	static List<Card> deck = new List<Card>();
+	static Dictionary<string, string> symbol = new Dictionary<string, string>();
+	static Dictionary<string, string> letter = new Dictionary<string, string>();
+	
         static void Main(string[] args)
         {
 
@@ -18,27 +21,42 @@ namespace HighOrLow
             // Value test = Value.king;
             // Console.WriteLine(test);
 
-            Dictionary<string, string> symbol = new Dictionary<string, string>();
-            Dictionary<string, string> letter = new Dictionary<string, string>();
+
 
             // var suit = RandomEnumValue<Suit>();
             // var value = RandomEnumValue<Value>();
 
-            Random rnd = new Random();
+	    Random rnd = new Random();
+	    
+            string[] suits = new string[4];
+
+            suits[0] = "Clubs";
+            suits[1] = "Diamonds";
+            suits[2] = "Hearts";
+            suits[3] = "Spades";
+
+            var suit = suits[rnd.Next(0, 3)];
+	    
+
+
+
+	    
+
+	    
+
 
             int temp = rnd.Next(0, 10);
-            Console.WriteLine(temp);
+            // Console.WriteLine(temp);
 
-            Console.WriteLine((Value)temp-1);
+            // Console.WriteLine((Value)temp-1);
             createDeck();
-            Console.WriteLine(deck[0].getRank());
+            // Console.WriteLine(deck[0].getRank());
 
             // Console.WriteLine(value);
 
             // Console.WriteLine (value.ToString () +" of " + suit.ToString());
 
-
-
+	    
             symbol.Add("Hearts", "\u2665");
 	    symbol.Add("Clubs", "\u2666");
 	    symbol.Add("Diamonds", "\u2663");
@@ -49,9 +67,22 @@ namespace HighOrLow
 
             // Console.WriteLine(value. + symbol[suit.ToString()]);
 
-            Card test = new Card("test", "korv");
 
-            Console.WriteLine(test.getRank());
+
+            // for (int i = 0; i < deck.Count; ++i) {
+            //     Console.WriteLine(deck[i].getRank() + symbol[deck[i].getSuit().ToString()]);
+            // }
+
+            while (true)
+            {
+                getCard();
+                Console.ReadLine();
+            }
+
+
+            // Card test = new Card("test", "korv");
+
+            // Console.WriteLine(test.getRank());
 
         }
 
@@ -99,10 +130,35 @@ namespace HighOrLow
 
         static void createDeck()
         {
-            deck.Add(new Card("korv", "joel tengman"));
 
+	    for (int i = 0; i < 13; ++i) {
+                deck.Add(new Card(i.ToString(), "Diamonds"));
+            }
+
+	    for (int i = 0; i < 13; ++i) {
+                deck.Add(new Card(i.ToString(), "Clubs"));
+            }	    
+
+	    for (int i = 0; i < 13; ++i) {
+                deck.Add(new Card(i.ToString(), "Spades" ));
+            }	    
+
+	    for (int i = 0; i < 13; ++i) {
+                deck.Add(new Card(i.ToString(), "Hearts"));
+            }
+
+	    
+        }
+
+        static void getCard()
+        {
+	    Random rnd = new Random();
+            var cardNum = rnd.Next(0, deck.Count);
+	    Console.WriteLine(deck[cardNum].getRank() + symbol[deck[cardNum].getSuit().ToString()]);
+            deck.RemoveAt(cardNum);
 
         }
+
 
     }
 }
